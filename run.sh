e3bf66e26afe667cba894b2c03f217e5c5c2338c
Ryan Gregg <github@ryangregg.com>
2024-09-16 23:54:58 -0700

    Update run script with changes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t mqtt-logger
-docker run --rm -v ./logs:/logs -e MQTT_BROKER=hestia.lan -e MQTT_PORT=1883 -e MQTT_TOPIC=frigate/events -e LOG_FILE=/logs/mqtt_logs.txt mqtt-logger
+docker build . -t frigate-event-processor
+docker run --rm -v ./logs:/app/logs -v ./config.yaml:/app/config.yaml:ro frigate-event-processor
